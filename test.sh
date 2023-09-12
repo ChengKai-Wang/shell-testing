574fd9140f980e85a2b2e56a71e8f0550040e2dd
Cheng-Kai Wang <ckwang9805@gmail.com>
2023-09-12 09:48:38 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,7 @@
-echo "THIS IS A SIMPLE TEST FOR SHELL"
+echo "THIS IS A SAMPLE SHELL SCRIPT"
+if [ "$TEST_VAR"!="TEST" ]; then
+    TEST_VAR="TEST"
+    echo "TEST_VAR is set to TEST"
+ else
+     echo "TEST_VAR is already set to TEST"
+fi
